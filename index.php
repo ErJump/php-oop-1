934464fb4a1d5d3fec98e3769699a598d5fa9082
ErJump <fedelijunior@gmail.com>
2022-09-02 13:39:32 +0200

    feat: :sparkles: Adds two instances of movie class
    Ex. completed

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+    require_once __DIR__ . '/Classes/Movie.php';
+
+    $movieOne = new Movie("Fight Club", 1999, "David Fincher");
+    $movieTwo = new Movie("The Big Lebowski", 1998, "Joel Coen & Ethan Coen");
+    var_dump($movieOne);
+    var_dump($movieTwo);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +15,18 @@
     <title>OOP 1</title>
 </head>
 <body>
-    
+    <h1>Movies:</h1>
+    <ul>
+        <ul>
+            <h2><?php echo $movieOne->getTitle(); ?></h2>
+            <li><?php echo $movieOne->getYear(); ?></li>
+            <li><?php echo $movieOne->getDirector(); ?></li>
+        </ul>
+        <ul>
+            <h2><?php echo $movieTwo->getTitle(); ?></h2>
+            <li><?php echo $movieTwo->getYear(); ?></li>
+            <li><?php echo $movieTwo->getDirector(); ?></li>
+        </ul>
+    </ul>
 </body>
 </html>
